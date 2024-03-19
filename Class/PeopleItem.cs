387d08ea2e343c7ac8e0d654385e6e6c69f5f4a6
Toei <hs4toei2544@gmail.com>
2024-03-19 08:29:31 +0700

    People1

---
+++ Class/PeopleItem.cs
@@ -0,0 +1,6 @@
+public class PeopleItem
+{
+    public string? Title { get; set;}
+    //public int? Integer   {get; set;}
+    public bool IsDone {get; set;}
+}
---
+++ Class/PeopleSex.cs
@@ -0,0 +1,6 @@
+public class PeopleSex
+{
+    public string? Title { get; set;}
+    //public int? Integer   {get; set;}
+    public bool IsDone {get; set;}
+}
